7fa79eb75dd4f83fa475af8a80a95d987f5b81a3
Paul Martin <codemobius@gmail.com>
2016-02-14 13:49:33 -0500

    updated switch to use mqtt

--- switch.js
+++ switch.js
@@ -1,4 +1,26 @@
+client = new Paho.MQTT.Client("test.mosquitto.org", 8080, "clientId");
+client.connect({onSuccess:onConnect});
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("/light-123");
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+  var s = document.getElementById("switch");
+  if ( message.payloadString == "on"){
+    s.checked = true;
+  } else if (message.payloadString =="off"){
+    s.checked = false;
+  }
+}
+
 function toggle(s){
   var value = s.checked;
-  window.location = "/?switch=" + (value ? "on" : "off");
+  message = new Paho.MQTT.Message(value ? "on" :"off");
+  message.destinationName = "/light-123
+  client.send(message); 
 }
